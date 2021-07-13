89509fbad004d2099f0fb6452e3123d8751793dd
ikopeykin <5349983@gmail.com>
2021-07-14 00:25:52 +0300

    Developed easy solution of row borders

--- src/components/statusBar.tsx
+++ src/components/statusBar.tsx
@@ -7,14 +7,23 @@ interface StatusBarProps {
 }
 
 export const StatusBar = ({status}: StatusBarProps) => {
+    console.log(Status[status]);
+
     return (
         <div className="statusBar">
-            <ul className="statusBarList">
-                <li className="statusBarElement firstBarElement">ADDED</li>
-                <li className="statusBarElement secondBarElement">IN-CHECK</li>
-                <li className="statusBarElement thirdBarElement">APPROVED</li>
-                <li className="statusBarElement fourthBarElement">ACTIVE</li>
-                <li className="statusBarElement lastBarElement">INACTIVE</li>
+            {/*<ul className="statusBarList">*/}
+            {/*    <li className="statusBarElement firstStatusBarElement">ADDED</li>*/}
+            {/*    <li className="statusBarElement middleStatusBarElement">IN-CHECK</li>*/}
+            {/*    <li className="statusBarElement middleStatusBarElement">APPROVED</li>*/}
+            {/*    <li className="statusBarElement middleStatusBarElement">ACTIVE</li>*/}
+            {/*    <li className="statusBarElement lastStatusBarElement">INACTIVE</li>*/}
+            {/*</ul>*/}
+            <ul className="testStatus">
+                <li className="testStatus">ADDED</li>
+                <li className="testStatusNoGood">IN-CHECK</li>
+                <li className="testStatusNoGood">APPROVED</li>
+                <li className="testStatusNoGood">ACTIVE</li>
+                <li className="testStatus">INACTIVE</li>
             </ul>
         </div>
     )
